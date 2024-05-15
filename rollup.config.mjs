4c146403b4805e510ef081c4398bbe9608e86baa
jarvisjiang <jarvisjiang@tencent.com>
2024-05-15 17:52:15 +0800

    use esbuild plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,33 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import esbuild from 'rollup-plugin-esbuild';
+
 /**
  * @type {import('rollup').RollupOptions}
  */
-export default
-    {
-        input: 'dist/esbuild/index.js',
-        output: [
-            {
-                file: 'dist/rollup/index.js',
-                format: 'esm',
-                sourcemap: false,
-            },
-        ],
-        external: [
-            'minigame-std',
-        ],
-        treeshake: 'smallest',
-    };
+export default {
+    input: 'src/index.ts',
+    plugins: [
+        esbuild({
+            target: 'esnext',
+        }),
+        replace({
+            preventAssignment: true,
+            __MINIGAME_STD_MINA__: `true`,
+        }),
+        nodeResolve(),
+        commonjs(),
+    ],
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'esm',
+            sourcemap: false,
+        },
+    ],
+    treeshake: {
+        unknownGlobalSideEffects: false,
+        propertyReadSideEffects: false,
+    },
+};
